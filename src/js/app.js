8b16073cfed0c92f1109ffa409f94b867402af25
Bozhydara Bozhanova <101105150+BojidaraBojanova@users.noreply.github.com>
2022-09-21 15:37:27 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
-import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+ 
+  let article = document.createElement('article');
+  article.className='message';
+  article.textContent = 'hello';
+  article.className='message';
+  article.textContent = 'hello';
+  article.className='message';
+  article.textContent = 'hello';
+  article.className='message';
+  article.textContent = 'hello';
+  article.className='message';
+  article.textContent = 'hello';
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
